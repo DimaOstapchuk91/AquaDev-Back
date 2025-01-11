25863cce14749184bdebd1df97587165c6d1d02e
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2025-01-11 14:58:38 +0200

    add patch request in cors

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ export const setupServer = () => {
 
   const corsOptions = {
     origin: ['http://localhost:5173', 'https://aqua-dev-amber.vercel.app'],
-    methods: 'GET,POST,PUT,DELETE',
+    methods: 'GET,POST,PATCH,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
   };
